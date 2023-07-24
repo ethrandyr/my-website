d6753b63bdbf0e38e88ba617b5e0cc585c59e40d
ethrandyr <136713025+ethrandyr@users.noreply.github.com>
2023-07-25 02:16:55 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,37 @@
+// scripts.js
 const bookList = document.getElementById('bookList');
 
-function addBook() {
-    const bookTitle = document.getElementById('bookTitle').value;
-    const author = document.getElementById('author').value;
-
-    if (bookTitle.trim() !== '' && author.trim() !== '') {
-        const bookItem = document.createElement('li');
-        bookItem.innerHTML = `<strong>${bookTitle}</strong> by ${author}`;
-        bookList.appendChild(bookItem);
-
-        document.getElementById('bookTitle').value = '';
-        document.getElementById('author').value = '';
-    }
+function saveBookData(bookTitle, author) {
+  const existingData = JSON.parse(localStorage.getItem('bookData')) || [];
+  const newData = {
+    bookTitle: bookTitle,
+    author: author,
+  };
+  existingData.push(newData);
+  localStorage.setItem('bookData', JSON.stringify(existingData));
 }
+
+function displayBookData() {
+  const savedData = JSON.parse(localStorage.getItem('bookData')) || [];
+  bookList.innerHTML = '';
+  savedData.forEach((data) => {
+    const bookItem = document.createElement('li');
+    bookItem.innerHTML = `<strong>${data.bookTitle}</strong> by ${data.author}`;
+    bookList.appendChild(bookItem);
+  });
+}
+
+function addBook() {
+  const bookTitle = document.getElementById('bookTitle').value;
+  const author = document.getElementById('author').value;
+
+  if (bookTitle.trim() !== '' && author.trim() !== '') {
+    saveBookData(bookTitle, author);
+    displayBookData();
+
+    document.getElementById('bookTitle').value = '';
+    document.getElementById('author').value = '';
+  }
+}
+
+window.onload = displayBookData;
